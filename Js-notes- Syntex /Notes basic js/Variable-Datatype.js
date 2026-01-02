9d8073a889fc9a8cd42d8afc7c6f982e0315a62b
Pawankumaryadav <pawan902670@gmail.com>
2026-01-02 18:59:58 +0530

    Create Variable-Datatype.js
    Variable and Data type

---
+++ Js-notes- Syntex /Notes basic js/Variable-Datatype.js
@@ -0,0 +1,15 @@
+ var name = "Pawan";
+var age = 22;
+pawankumaryadav: let city = "Delhi";
+city = "Mumbai";
+ pawankumaryadav: const country = "India";
+typeof 10;         // "number"
+typeof "Hello";    // "string"
+typeof true;       // "boolean"
+typeof undefined;  // "undefined"
+typeof null;       // "object"  // JavaScript bug
+let x = 10;
+x = "Hello";
+x = true;
+let name = "Pawan";
+console.log(`Hello ${name}`);
