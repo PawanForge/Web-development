bb075bc716c6bba1185285e27a78c78acd999e12
Pawankumaryadav <pawan902670@gmail.com>
2026-01-15 14:01:46 +0530

    Create TransactionComponent.js

---
+++ Expense Tracker/TransactionComponent.js
@@ -0,0 +1,16 @@
+import React from "react";
+import styled from "styled-components";
+const Container=styled.div`
+display:flex;
+flex-direction:column;
+align-items:center;
+margin:30px 0 10px;
+font-family:sense-serif;
+`;
+const  TransactionComponent=(props)=>{
+return(
+  <Container>Transaction component 
+  </Container>
+);
+}
+export default TransactionComponent;
