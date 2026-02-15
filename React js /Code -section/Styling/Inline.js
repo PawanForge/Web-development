9b92b892d07a277cae90f557a225285d5d67fdd5
Pawankumaryadav <pawan902670@gmail.com>
2026-02-15 14:10:41 +0530

    Create Inline.js

---
+++ React js /Code -section/Styling/Inline.js
@@ -0,0 +1,19 @@
+function App() {
+
+  const inlineStyle = {
+    textAlign: "center",
+    backgroundColor: "skyblue",
+    padding: "20px",
+    marginBottom: "20px"
+  };
+
+  return (
+    <div>
+      <div style={inlineStyle}>
+        <h2>Inline CSS Example</h2>
+      </div>
+    </div>
+  );
+}
+
+export default App;
