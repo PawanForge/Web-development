1d2c0f575eb3d12b097378bc6c589639e8aee3d4
Pawankumaryadav <pawan902670@gmail.com>
2026-05-04 21:46:41 +0530

    Create React-Router.js

---
+++ React js /Code -section/React-Router.js
@@ -0,0 +1,18 @@
+import { BrowserRouter,Route,Routes,Link } from "react-router";
+export default function App(){
+  return(
+    <div>
+      <BrowserRouter>
+      <Link to="/" >Home</Link>
+      <Link to="/about">About</Link>
+      <Link to="/contact">Contact</Link>
+    
+      <Routes>
+        <Route path="/" element={<h1>Home</h1>}/>
+        <Route path="/about" element={<h1>Home</h1>}/>
+        <Route path="/contact" element={<h1>Home</h1>}/>
+      </Routes>
+      </BrowserRouter>
+    </div>
+  )
+}
